74d9e87441bd753eab366c5335f20959d9707035
Anton Simonov <asimonov215@gmail.com>
2023-01-22 07:39:47 +0300

    Solving assignment 1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,17 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn display_first_name(f_name: &str) {
+    println!("My first name is: {}", f_name);
+}
+
+fn display_second_name(s_name: &str) {
+    println!("My second name is: {}", s_name);
+}
+
+fn main() {
+    let first_name = "Simon";
+    let second_name = "Sun";
+    display_first_name(first_name);
+    display_second_name(second_name);
+}
