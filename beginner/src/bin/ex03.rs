59dbec8cfa1682dbe3af79775db9d118e52358eb
Anton Simonov <asimonov215@gmail.com>
2023-01-22 07:55:36 +0300

    Refactor

---
+++ beginner/src/bin/ex03.rs
@@ -0,0 +1,15 @@
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+fn main() {
+    let sum = 2 + 20;
+    let value = 29 - 8;
+    let division = 10 / 3;
+    let mult = 83 * 83;
+
+    let five = sub(8, 3);
+
+    let rem = 6 % 3;
+    let rem2 = 6 % 4;
+}
